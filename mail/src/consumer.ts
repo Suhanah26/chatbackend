277dbf0f418f4128fc94462160c7ed3f9cd0ca2e
Suhana Hussain <123799400+Suhanah26@users.noreply.github.com>
2026-01-15 20:46:37 +0530

    Update consumer.ts

--- mail/src/consumer.ts
+++ mail/src/consumer.ts
@@ -22,6 +22,7 @@ export const startsendOTPConsumer = async () => {
           const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
             port: 465,
+            secure: true,   
             auth: {
               user: process.env.USER,
               pass: process.env.PASSWORD,
